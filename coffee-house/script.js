97cc797a597e968f23cce1501266ea5f42b2ba2e
Angelina Baranova <gerlinda137@gmail.com>
2023-12-03 19:23:53 +0400

    feat: responsive header

--- coffee-house/script.js
+++ coffee-house/script.js
@@ -1,6 +1,10 @@
-const burger = document.querySelector(".burger");
-const navWrapper = document.querySelector(".nav__wrapper");
+const header = document.querySelector(".header");
+const burger = header.querySelector(".burger");
+const navWrapper = header.querySelector(".nav__wrapper");
+const body = document.querySelector("body");
+
 burger.onclick = () => {
   navWrapper.classList.toggle("open");
   burger.classList.toggle("cross");
+  body.classList.toggle("no-scroll");
 };
