0d5ea303788652133c48c6da6705b93b6aa12a8b
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2025-10-27 16:20:11 +0400

    feat: set up deploy

--- coffee-house/vite.config.js
+++ coffee-house/vite.config.js
@@ -12,8 +12,17 @@ export default defineConfig({
   server: {
     port: 5173,
   },
+  // base: "./",
+  base: "/rsschool-shorttrack/coffee-house/",
   build: {
-    outDir: "../dist",
+    outDir: "../dist/coffee-house",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "src/index.html",
+        menu: "src/menu.html",
+        cart: "src/cart.html",
+      },
+    },
   },
 });
